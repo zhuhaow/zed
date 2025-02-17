0d58699a4444f6a446080cf9261db6d80f04f76c
Junkui Zhang <364772080@qq.com>
2025-02-17 16:31:38 +0800

    Use `CreateMutex` for single instance

--- crates/zed/src/zed/windows_only_instance.rs
+++ crates/zed/src/zed/windows_only_instance.rs
@@ -3,28 +3,27 @@ use windows::{
     core::HSTRING,
     Win32::{
         Foundation::{GetLastError, ERROR_ALREADY_EXISTS},
-        System::Threading::CreateEventW,
+        System::Threading::CreateMutexW,
     },
 };
 
 fn retrieve_app_instance_event_identifier() -> &'static str {
     match *release_channel::RELEASE_CHANNEL {
-        ReleaseChannel::Dev => "Local\\Zed-Editor-Dev-Instance-Event",
-        ReleaseChannel::Nightly => "Local\\Zed-Editor-Nightly-Instance-Event",
-        ReleaseChannel::Preview => "Local\\Zed-Editor-Preview-Instance-Event",
-        ReleaseChannel::Stable => "Local\\Zed-Editor-Stable-Instance-Event",
+        ReleaseChannel::Dev => "Local\\Zed-Editor-Dev-Instance-Mutex",
+        ReleaseChannel::Nightly => "Local\\Zed-Editor-Nightly-Instance-Mutex",
+        ReleaseChannel::Preview => "Local\\Zed-Editor-Preview-Instance-Mutex",
+        ReleaseChannel::Stable => "Local\\Zed-Editor-Stable-Instance-Mutex",
     }
 }
 
 pub fn check_single_instance() -> bool {
     unsafe {
-        CreateEventW(
+        CreateMutexW(
             None,
-            false,
             false,
             &HSTRING::from(retrieve_app_instance_event_identifier()),
         )
-        .expect("Unable to create instance sync event")
+        .expect("Unable to create instance sync mutex")
     };
     let last_err = unsafe { GetLastError() };
     last_err != ERROR_ALREADY_EXISTS
